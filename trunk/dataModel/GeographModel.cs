f34783290ef6998ab6d50d1a98407ff885277c0d
v.keating@student.qut.edu.au <v.keating@student.qut.edu.au@43097454-98f9-11de-a05d-3bd73e269465>
2009-09-09 01:41:02 +0000

    First draft GeographModel and test

--- trunk/dataModel/GeographModel.cs
+++ trunk/dataModel/GeographModel.cs
@@ -3,76 +3,172 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using log4net;
+using log4net.Config;
 
 namespace BlackCat
 {
     //this class stores the [ region ] information found in both mid and mif files
     public class GeographModel : IGeographModel
     {
-        //Contains the list of column names derived from the original .mid file
-        private List<String> dataFieldNames;
-        //The list of regions with their coordinates
-        private List<GeographicRegion> electorates;
-
-        private class GeographicRegion
-        {
+        //data members
+        private int dataCount;
+        private String[] dataHeaders;
+		private char dataDelim;
+        private List<Region> regions;
+        private ILog log;
         
-            //Name of the region
-            private string regionName;
-            //Contains the colour that the region will eventually be depicted with
-            private string colorCode;
-            //The X coordinate for the region.
-             private string X;
-            //The Y coordinate for the region.
-            private string Y;
+        //Constructors
+        public GeographModel()
+        {
+            initialiseLogging();
+        }
 
-            GeographicRegion() { }
+        //private methods
+        private void initialiseLogging()
+        {
+            BasicConfigurator.Configure();  
+            log = LogManager.GetLogger( typeof( GeographModel ) );
+        }
 
-            //Properties
-            //TO BE IMPLEMENTED YOURSELF
-
-            /* SAMPLE PROPERTY METHOD
-             * 
-             public string myRegionName
+        //public methods
+        public void buildGeographModel(StreamReader midReader, StreamReader mifReader)
+        {
+            log.Debug("Beginning the build process");
+			String line;
+			String[] lineParts;
+			//Read header information
+            dataDelim = '\t'; //tab is the default delimiter
+			while(!(line = mifReader.ReadLine()).ToUpper().StartsWith("COLUMNS")) //Version
+			{
+				if(line.ToUpper().StartsWith("DELIMITER"))
+				{
+					lineParts = line.Split('"');
+					dataDelim = Char.Parse(lineParts[lineParts.Length-1]);
+				}
+			}
+			
+			//Column info
+			lineParts = line.Split(' ');
+            dataCount = int.Parse(lineParts[1]);
+			dataHeaders = new String[dataCount];
+			for(int i = 0 ; i < dataHeaders.Length ; i++)
+			{
+				line = mifReader.ReadLine();
+				lineParts = line.Split(' ');
+				dataHeaders[i] = lineParts[0];
+			}
+			
+			//Data Section
+			while((line = mifReader.ReadLine()) != null)
+			{
+				if(line.ToUpper().StartsWith("REGION"))
+				{
+					lineParts = line.Split(' ');					
+					ReadRegion(mifReader, int.Parse(lineParts[lineParts.Length - 1]));
+				}
+			}
+            mifReader.Close();
+        }
+		
+		private void ReadRegion(StreamReader mifReader,int polyCount)
+		{
+            log.Debug("Reading region");
+            String line;
+            String[] lineParts;
+            Region region = new Region(dataCount, polyCount);
+            for (int i = 0; i < polyCount; i++)
+            {
+                int coordCount = int.Parse(mifReader.ReadLine());
+                Coordinate[] polygon = new Coordinate[coordCount];
+                for (int j = 0; j < coordCount; j++)
                 {
-                    get { return this.regionName; }
-                    set { this.regionName = value; }
+                    line = mifReader.ReadLine();
+                    lineParts = line.Split(' ');
+                    float x = float.Parse(lineParts[0]);
+                    float y = float.Parse(lineParts[1]);
+                    polygon[j] = new Coordinate(x, y);
                 }
-            */
+                log.Debug("Adding polygon to region");
+                region.addPolygon(polygon);
+            }
+		}
+        
+		///
+		///Base class for all MapInfo elements
+		///
+        private abstract class MapElement
+        {
+            //data members
+            private String[] dataValues;
 
-            //string myRegionName{get; set};
-            //string coordinateX{get; set};
-            //string coordinateY{get; set};
+            //constructors
+            public MapElement(int dataCount)
+            {
+                this.dataValues = new String[dataCount];
+            }
 
+            //properties
+            public String[] DataValues
+            {
+                get { return dataValues; }
+                set { dataValues = value; }
+            }
+
+            //methods
+            public String getDataValue(int index)
+            {
+                if (index < dataValues.Length)
+                    return dataValues[index];
+                return null;
+            }
         }
 
-        //Populates the geographical data model from the reader objects for both the .mid and .mif files and returns it.
-        //Pre: midReader and mifReader both must not be null
-        //Post: The GeographModel the method is called on is populated with the data contained in the MapInfo files.
-        public void buildGeographModel(StreamReader midReader, StreamReader mifReader) { }
+        private class Region : MapElement
+        {
+            //data members
+            private Coordinate[][] coords; //TODO: better desc - first index is polygon, second is coordinate
+            private int currentPoly = 0;
 
-        //Adds a region to the list.
-        //Pre : Collection exists, regName is not null, regCoord is not null and colorCode is not null
-        //Post : A region has been added to the collection
-        public void addGeographicData(string regName, string regCoord, string colorCode) { }
-
-        //Count the number of regions in the collection
-        //Pre: Collection exists
-        //Post: An integer representing the number of regions in the collection is returned.
-        public int countGeographicRegions() {
-            return 0;
+            //constructors
+            public Region(int dataCount, int polygonCount)
+                : base(dataCount)
+            {
+                coords = new Coordinate[polygonCount][];
+            }    
+        
+            //methods
+            public void addPolygon(Coordinate[] coords)
+            {
+                this.coords[currentPoly] = coords;
+                currentPoly++;
+                if(currentPoly == coords.GetLength(0))
+                    currentPoly--;
+            }
         }
 
-        //Return a string of X and Y coordinates for the object at the given index
-        //Pre: Index must be less than the collection count, but at least zero.
-        //Post: A string of X and Y coordinates is returned (partitioned with a delimiter of | )
-        public String getGeographicCoord(int geoIndex) {
-            return "";
-        }
+        private class Coordinate
+        {
+            private float x;
+            private float y;
 
-        //Returns the GeographicRegion located at the given index – can be used to iterate
-        //Pre: Index must be less than the collection count, but at least zero.
-        //Post: The GeographicRegion at geoIndex is returned.
-        //public GeographicRegion getGeograpicRegion(int geoIndex) { }
+            public Coordinate(float x, float y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+
+            public float X
+            { 
+                get { return this.x; } 
+                set { this.x = value; } 
+            }
+
+            public float Y
+            {
+                get { return this.y; }
+                set { this.y = value; }
+            }
+        }
     }
 }
