15cf24b760c2f7c9e5fb3e6e42cadaae2ad41fab
v.keating@student.qut.edu.au <v.keating@student.qut.edu.au@43097454-98f9-11de-a05d-3bd73e269465>
2009-09-10 05:47:21 +0000

    IValidator interface updated

--- Validator/IValidator.cs
+++ Validator/IValidator.cs
@@ -7,10 +7,6 @@ namespace BlackCat
 {
     public interface IValidator
     {
-        //check the file type.
-        //pre: filePath
-        //post: 
-        bool validationFileType(string filePath, string fileType);
         // Check the given drive for the sufficient space to write the file
         //Pre: drive exists and the string drive is not the empty string
         //Post: Returns true iff there is sufficient space.
@@ -36,22 +32,53 @@ namespace BlackCat
         //Post : Returns true iff the file could be found at the givel location
         bool fileExists(String fileURL);
 
-        //Tests if the given Excel file has the correct format
-        //Pre : excelURL must exist and excelURL is not the empty string
-        //Post : Returns true iff file structure corresponds to that expected for an Excel file
-        bool validateExcelFileFormat(String excelURL);
-        //bool validateFERQExcelFormat(String excelURL);
-        //bool validateQFREExcelFormat(String excelURL);
+        /// <summary>
+        /// Checks that the url is not too long to be written to by .NET.
+        /// </summary>
+        /// <param name="fileURL"></param>
+        /// <returns></returns>
+        bool urlLengthIsValid(String fileURL);
+
+        /// <summary>
+        /// Tests if the format of the file is valid. 
+        /// That is, it has a format consistant with "Federal Election Results-Qld-2004.xls"
+        /// 
+        /// Pre : excelURL must exist and excelURL is not the empty string
+        /// Post : Returns true iff file structure corresponds to that expected for an Excel file
+        /// </summary>
+        /// <param name="excelURL">The url of the excel file</param>
+        /// <returns>true if the file is valid, else returns false</returns>
+        bool validateFERQExcelFormat(String excelURL);
+
+        /// <summary>
+        /// Tests if the format of the file is valid. 
+        /// That is, it has a format consistant with "Qld_FederalResults by Electorate-2004.xls"
+        /// 
+        /// Pre : excelURL must exist and excelURL is not the empty string
+        /// Post : Returns true iff file structure corresponds to that expected for an Excel file
+        /// </summary>
+        /// <param name="excelURL">The url of the excel file</param>
+        /// <returns>true if the file is valid, else returns false</returns>
+        bool validateQFREExcelFormat(String excelURL);
 
         // Tests if the given .mid file has the correct format
         //Pre : midURL exists and midURL is not the empty string
         //Post : Returns true iff file structure corresponds to that expected for a .mid file.
-        bool validateMidFormat(String mifURL, String midURL);
+        bool validateMidFormat(String midURL);
 
         //Tests if the given .mif file has the correct format
         //Pre : mifURLexists and mifURL is not the empty string
         //Post : Returns true iff file structure corresponds to that expected for a .mif file
-        bool validateMifFormat(String mifURL, String midURL);
+        bool validateMifFormat(String mifURL);
+
+        /// <summary>
+        /// Checks if the mid and mif files match each other -
+        /// that the columns count and data count match.
+        /// </summary>
+        /// <param name="mifURL"></param>
+        /// <param name="midURL"></param>
+        /// <returns></returns>
+        bool validateMapInfoFormat(String mifURL, String midURL);
 
         //Tests if the given KML file has the correct format
         //Pre : kmlURL exists and kmlURL is not the empty string
