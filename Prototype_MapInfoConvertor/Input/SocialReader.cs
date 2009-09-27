2b16b91f3deebf3ab5d04ecfe932bec2b45481b9
v.keating@student.qut.edu.au <v.keating@student.qut.edu.au@43097454-98f9-11de-a05d-3bd73e269465>
2009-09-27 11:43:59 +0000

    Some files moved, small edits made to conform to specs.

---
+++ Prototype_MapInfoConvertor/Input/SocialReader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlackCat
+{
+    public class SocialReader : ISocialReader
+    {
+        String[] getStateSeats(String federalSeat)
+        {
+            //TODO: implement
+            return null;
+        }
+
+        StateElectorateData GetStateResults(string stateSeat)
+        {
+            //TODO: implement
+            return null;
+        }
+
+        FederalElectorateData GetFederalResults(string federalSeat)
+        {
+            //TODO: implement
+            return null;
+        }
+    }
+}
