1487d1eaf73a6597b18ed22c3dc4840a386f64d8
v.keating@student.qut.edu.au <v.keating@student.qut.edu.au@43097454-98f9-11de-a05d-3bd73e269465>
2009-10-07 09:51:41 +0000

    Old database removed. All Interfaces made current with the Design Spec. Validation code moved to BlackCatKMLParser/Gui/BlackCatParserUI.cs.

--- BlackCatKMLParser/Controller/IKMLParserControl.cs
+++ BlackCatKMLParser/Controller/IKMLParserControl.cs
@@ -6,51 +6,70 @@ namespace BlackCat
 {
     public interface IKMLParserControl
     {
-        // Creates the KML file using the files previously supplied by the user, writing it to the 
-        // location desired and arranging the updating of a progress bar as the operation proceeds. 
-        // Returns an integer denoting success (0) or failure (1).
-
-        // Pre: outputFileURL is not the empty string and progressBar is not null.
-        // Post: A new KML file is written to outputFolderURL, created from the input files.
-
-        int GenerateKMLFile(String outputFileURL, ProgressBar progressBar); 
-        
-       
-        string[] GetMapInfoDataFields();
+        // Checks to see if any sociological data can be added to the geographical data.
+        // Returns a boolean – true if sociological data can be added, false if it can’t.
+        // Pre: True
+        // Post: A boolean has been returned indicating if sociological data can be added to 
+        // this set of geographical data.
 
         bool CanAddSociologicalData();
 
+        // Creates the KML file using the files previously supplied by the user, writing it to the 
+        // location desired and arranging the updating of a progress bar as the operation 
+        // proceeds. 
+        // Returns an integer denoting success (0) or failure (1).
+        // Pre: outputFileURL is not the empty string progressBar is not  null
+        // Post: A new KML file is written to outputFolderURL, created from the input files.
 
-        // Loads the KML file fileURL into the system, arranging the updating of a progress bar as the 
-        // operation proceeds. Returns an integer denoting the result of the operation as follows:
+        int GenerateKMLFile(String outputFileURL, ProgressBar progressBar);
 
-        // value = 0 (No problems) 
-        // value = 1 (file doesn’t exist)
-        // value = 2 (file is not readable)
-        // value = 3 (file is in the wrong format).
+        // Extracts the data fields from the MapInfo file so that the user can select one or more // of them to display in Google Earth.
+        // Returns a string array containing the names of the data fields present in the 
+        // MapInfo file.
+        // Pre: True
+        // Post: An array of field names corresponding to those found in the MapInfo file is 
+        // returned.
 
+        string[] GetMapInfoDataFields();
+
+        // Loads the KML file fileURL into the system, arranging the updating of a progress 
+        // bar as the operation proceeds. 
+        // Returns an integer denoting success (0) or failure (1).
         // Pre: fileURL is not the empty string and progressBar is not null.
-        // Post: fileURL has been loaded and an integer denoting success or otherwise has been returned.
+        // Post: fileURL has been loaded and an integer denoting success or otherwise has 
+        // been returned.
 
         int LoadKML(String fileURL, ProgressBar progressBar);
 
-        // Loads the MapInfo files midFileURL and mifFileURL into the system, arranging the updating of 
-        // a progress bar as the operation proceeds. Returns an integer denoting the result of the 
-        // operation as follows:
 
-        // value = 0 (No problems) 
-        // value = 1 (mid file doesn’t exist)
-        // value = 2 (mid file is not readable)
-        // value = 3 (mid file is in the wrong format)
-        // value = 4 (mif file doesn’t exist)
-        // value = 5 (mif file is not readable)
-        // value = 6 (mif file is in the wrong format).
-
+        // Loads the MapInfo files midFileURL and mifFileURL into the system, arranging 
+        // the updating of a progress bar as the operation proceeds. 
+        // Returns an integer denoting success (0) or failure (1).
         // Pre: midFileURL, mifFileURL are not empty strings and progressBar is not null.
-        // Post: midFileURL and mifFileURL have been loaded and an integer denoting success or otherwise 
-        // has been returned.
+        // Post: midFileURL and mifFileURL have been loaded and an integer denoting 
+        // success or otherwise has been returned.
 
         int LoadMapInfo(String midFileURL, String mifFileURL ,ProgressBar progressBar);
+
+        // Property to get and set the value of the mapInfoDataFieldsToDisplay attribute
+        // The getter returns the list of fields chosen by the user (can be empty and would be if // a KML file was used as the data source), the setter returns nothing.
+        // Pre: True 
+        // Post: The value of mapInfoDataFieldsToDisplay has been returned for getting or 
+        // the value of mapInfoDataFieldsToDisplay has been set to the input value for
+         // setting.
+
+        List<String> MapInfoDataFieldsToDisplay {get; set;}
+
+        // Property to get and set the value of the sociologicalDataChoice attribute
+        // The getter returns the sociological display option (NONE, WINNING_PARTY or 
+        // SEAT_SAFETY) currently set,  the setter returns nothing.
+        // Pre: True for the getter, value is one of NONE, WINNING_PARTY or 
+        // SEAT_SAFETY for the setter
+        // Post: The value of sociologicalDataChoice has been returned for getting or 
+        // the value of sociologicalDataChoice has been set to the input value for setting.
+
+        String SociologicalDataChoice {get; set;};
+
 
     }
 }
--- BlackCatKMLParser/DataModel/IRegion.cs
+++ BlackCatKMLParser/DataModel/IRegion.cs
@@ -3,8 +3,29 @@ namespace BlackCat
 {
     public interface IRegion
     {
-        string regionName { get; set; }
-        BlackCat.Style regionStyle { get; set; }
-        string regionType { get; set; }
+        // Property to get and set the value of the regionName attribute
+        // The getter returns the regionName, the setter returns nothing.
+        // Pre: True for getting, regionName is not null for setting
+        // Post: The value of regionName has been returned for getting or the value of 
+        // regionName has been set to the input value for setting.
+
+        String regionName { get; set; }
+
+        // Property to get and set the value of the regionStyle attribute
+        // The getter returns the regionStyle, the setter returns nothing.
+        // Pre: True for getting, regionStyle is not null for setting
+        // Post: The value of regionStyle has been returned for getting or the value of 
+        // regionStyle has been set to the input value for setting.
+
+        Style regionStyle { get; set; }
+
+        // Property to get and set the value of the regionType attribute
+        // The getter returns the regionType, the setter returns nothing.
+        // Pre: True for getting, regionType is not null for setting
+        // Post: The value of regionType has been returned for getting or the value of 
+        // regionType has been set to the input value for setting.
+
+        String regionType { get; set; }
+
     }
 }
