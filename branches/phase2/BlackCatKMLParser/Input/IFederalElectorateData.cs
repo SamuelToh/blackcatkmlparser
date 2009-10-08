b5907fbd2711731b55fcf09b794436ac7aaf1c32
s66.taylor@connect.qut.edu.au <s66.taylor@connect.qut.edu.au@43097454-98f9-11de-a05d-3bd73e269465>
2009-10-08 01:13:10 +0000

    IFederalElectorateData - changed clearAll to ClearAll in line with our coding standard

--- branches/phase2/BlackCatKMLParser/Input/IFederalElectorateData.cs
+++ branches/phase2/BlackCatKMLParser/Input/IFederalElectorateData.cs
@@ -96,7 +96,7 @@ namespace BlackCat
         // Post: All attributes of the FederalElectorateData object have been returned to null 
         // values.
 
-        void clearAll();
+        void ClearAll();
 
     }
 }
