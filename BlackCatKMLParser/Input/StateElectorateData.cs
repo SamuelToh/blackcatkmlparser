ad9a554c0e7900224c1f257b1a4c86356de83ba5
s66.taylor@connect.qut.edu.au <s66.taylor@connect.qut.edu.au@43097454-98f9-11de-a05d-3bd73e269465>
2009-10-08 01:10:02 +0000

    Added code for StateElectorateData Previous change was changed clearAll to ClearAll in line with our coding standard in IStateElectorateData

--- BlackCatKMLParser/Input/StateElectorateData.cs
+++ BlackCatKMLParser/Input/StateElectorateData.cs
@@ -7,23 +7,62 @@ namespace BlackCat
 {
     public class StateElectorateData : IStateElectorateData 
     {
-        private String name;
-        private String twoPartyPrefWinningParty;
+        private String stateElectorateName;
+        private String tPP_Winner_Party;
+
+
+        //Default constructor.
+
+        public StateElectorateData()
+        {
+            stateElectorateName = null;
+            tPP_Winner_Party = null;
+        }
+
+        // Property to get and set the value of the stateElectorateName attribute
+        // Pre: True for getting and setting.
+        // Post: The value of stateElectorateName has been returned for getting or the value of 
+        // stateElectorateName has been set to the input value for setting.
 
         public String StateElectorateName
         {
-            get { return name; }
-            set { name = value; }
+            get
+            {
+                return stateElectorateName;
+            }
+            set
+            {
+                stateElectorateName = value;
+            }
         }
+
+        // Property to get and set the value of the tPP_WinnerParty attribute
+        // Pre: True for getting and setting.
+        // Post: The value of tPP_WinnerParty has been returned for getting or the value of 
+        // tPP_WinnerParty has been set to the input value for setting.
 
         public String TPP_WinnerParty
         {
-            get { return twoPartyPrefWinningParty; }
-            set { twoPartyPrefWinningParty = value; }
+            get
+            {
+                return tPP_Winner_Party;
+            }
+            set
+            {
+                tPP_Winner_Party = value;
+            }
         }
 
-        public void clearAll()
+        // Resets all fields in the StateElectorateData object to null values.
+        // No return value.
+        // Pre: True.
+        // Post: All attributes of the StateElectorateData object have been returned to null values.
+
+        public void ClearAll()
         {
+            stateElectorateName = null;
+            tPP_Winner_Party = null;
         }
+
     }
 }
