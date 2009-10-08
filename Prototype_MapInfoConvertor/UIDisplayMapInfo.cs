43f3f4a61936b7e71fb167f0024062ca13c458d1
Tendious@Gmail.com <Tendious@Gmail.com@43097454-98f9-11de-a05d-3bd73e269465>
2009-10-08 08:55:16 +0000

    New UI for phase II

---
+++ Prototype_MapInfoConvertor/UIDisplayMapInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Prototype_MapInfoConvertor
+{
+    public partial class UIDisplayMapInfo : Form
+    {
+        public UIDisplayMapInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            UISelectSociologicalData socio = new UISelectSociologicalData();
+            socio.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ Prototype_MapInfoConvertor/UISelectSociologicalData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Prototype_MapInfoConvertor
+{
+    public partial class UISelectSociologicalData : Form
+    {
+        public UISelectSociologicalData()
+        {
+            InitializeComponent();
+        }
+
+        private void UISelectSociologicalData_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
