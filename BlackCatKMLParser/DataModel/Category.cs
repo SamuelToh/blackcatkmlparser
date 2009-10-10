2bf803f1bbe50ddcc46badc7e6a8a1efc7e3e119
Tendious@Gmail.com <Tendious@Gmail.com@43097454-98f9-11de-a05d-3bd73e269465>
2009-10-10 21:59:58 +0000

    Each region object will be categorized into different zones (e.g. NorthernQueensland) in google earth. This class is there so that when outputting the region data , the KML writer could differentiate each region's zone.

---
+++ BlackCatKMLParser/DataModel/Category.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlackCat
+{
+    public class Category : ICategory
+    {
+        private String categoryName;
+        private String categoryDesc;
+
+        //Constructor
+        public Category() { }
+
+        public Category(string categoryName,
+                            string categoryDesc)
+        {
+            this.categoryName = categoryName;
+            this.categoryDesc = categoryDesc;
+        }
+
+        //Properties
+        public String CategoryName
+        {
+            get { return this.categoryName; }
+            set { this.categoryName = value; }
+        }
+
+        //prop
+        public String CategoryDesc
+        {
+            get { return this.categoryDesc; }
+            set { categoryDesc = value; }
+        }
+    }
+}
